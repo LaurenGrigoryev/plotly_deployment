e375e4a8d095483e38b7cd0e87cd6060d1ef4b7c
LaurenSonis <75095346+LaurenSonis@users.noreply.github.com>
2021-02-28 21:31:56 -0500

    Update charts.js.js

--- JS/charts.js.js
+++ JS/charts.js.js
@@ -186,19 +186,4 @@ var filterMetaData = metadata.filter(sampleObj => sampleObj.id == sample)
     Plotly.newPlot('gauge', gaugeData, gaugeLayout);
   });
 }
-      // Initialize the dashboard
-      function init() {
-  var selector = d3.select("#selDataset");
-
-  d3.json("samples.json").then((data) => {
-    console.log(data);
-    var sampleNames = data.names;
-    sampleNames.forEach((sample) => {
-      selector
-        .append("option")
-        .text(sample)
-        .property("value", sample);
-    });
-})}
-
-init();
+      
